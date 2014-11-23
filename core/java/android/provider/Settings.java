2924f58a3d99dc5e66e845f967c9bf2a70c65719
Roman Birg <roman@cyngn.com>
2014-11-23 06:54:06 -0800

    SystemUI: double tap status bar to sleep (1/2).
    * Make it more reliable
    * Add it to keyguard
    * Add a content observer to not always query Settings.System on every
    touch event
    
    Change-Id: I292c4d9d9f810843590b7a9ec6e15b99ac44009d
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5306,6 +5306,12 @@ public final class Settings {
         public static final String NOTIFICATION_CLEAR_ALL_ICON_COLOR = "notification_clear_all_icon_color";
 
         /**
+         *  Enable statusbar double tap gesture on to put device to sleep
+         * @hide
+         */
+        public static final String DOUBLE_TAP_SLEEP_GESTURE = "double_tap_sleep_gesture";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
