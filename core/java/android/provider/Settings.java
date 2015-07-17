fb6df9e218c1f89bd68077c7f1d311ac95121169
Roman Birg <roman@cyngn.com>
2015-07-17 15:18:28 -0700

    SystemUI: attach a queue to media player notifications
    Extend media player notificatoins to attach a queue below them, set via
    the MediaController.setQueue() API, when available.
    
    Long pressing on the notificaiton will reveal an option to disable the
    queue.
    
    The queue is currently hardcoded to be limited to 3 items.
    
    Ticket: CYNGNOS-217
    Change-Id: I3b8503cb47af2d36f031d06aefc1bfbe7ed80010
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6899,6 +6899,13 @@ public final class Settings {
         public static final String MASTER_FONT_STYLE = "master_font_style";
 
         /**
+         * Whether to attach a queue to media notifications.
+         * 0 = 0ff, 1 = on
+         * @hide
+         */
+        public static final String NOTIFICATION_PLAY_QUEUE = "notification_play_queue";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
