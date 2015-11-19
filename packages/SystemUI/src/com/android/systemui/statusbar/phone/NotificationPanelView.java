5e5f27d1d32592de86e227607de03f9a1077653a
rogersb11 <brettrogers11@gmail.com>
2015-11-19 13:43:28 -0500

    Forward port kickass battery options 2/2

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -1846,7 +1846,6 @@ public class NotificationPanelView extends PanelView implements
 
     private void setListening(boolean listening) {
         mHeader.setListening(listening);
-        mKeyguardStatusBar.setListening(listening);
         mQsPanel.setListening(listening);
     }
 
