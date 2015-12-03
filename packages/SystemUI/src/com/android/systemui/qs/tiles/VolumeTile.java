ujwalp15/platform_frameworks_base

QS: Added show volume panel tile
Change-Id: I4115c92927a05ebb15edb581208fcfca18fbb52f

packages/SystemUI/src/com/android/systemui/qs/tiles/VolumeTile.java
@@ -1,0 +1,71 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.qs.tiles;
+
+import android.content.Context;
+import android.content.Intent;
+import android.media.AudioManager;
+import android.provider.Settings;
+
+import com.android.internal.logging.MetricsLogger;
+import com.android.systemui.R;
+import com.android.systemui.qs.QSTile;
+
+public class VolumeTile extends QSTile<QSTile.BooleanState> {
+
+    private static final Intent SOUND_SETTINGS = new Intent("android.settings.SOUND_SETTINGS");
+
+    public VolumeTile(Host host) {
+        super(host);
+    }
+
+    @Override
+    protected void handleClick() {
+        MetricsLogger.action(mContext, getMetricsCategory());
+        AudioManager am = (AudioManager) mContext.getSystemService(Context.AUDIO_SERVICE);
+        am.adjustVolume(AudioManager.ADJUST_SAME, AudioManager.FLAG_SHOW_UI);
+    }
+
+    @Override
+    protected void handleLongClick() {
+        mHost.startActivityDismissingKeyguard(SOUND_SETTINGS);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsLogger.QS_VOLUME;
+    }
+
+    @Override
+    protected void handleUpdateState(BooleanState state, Object arg) {
+        state.visible = true;
+        state.label = mContext.getString(R.string.quick_settings_volume_panel_label);
+        state.contentDescription = mContext.getString(
+                R.string.accessibility_quick_settings_volume_panel);
+        state.icon = ResourceIcon.get(R.drawable.ic_qs_volume_panel); // TODO needs own icon
+    }
+
+    @Override
+    protected BooleanState newTileState() {
+        return new BooleanState();
+    }
+
+    @Override
+    public void setListening(boolean listening) {
+        // Do nothing
+    }
+}

SuperKangerRom/android_frameworks_base-AOSP

Change-Id: Ic583c24e304d1edc903127237fc9eb5c7eeb7e4d
Conflicts:
	core/java/com/android/internal/logging/MetricsConstants.java
	packages/SystemUI/res/values/custom_strings.xml
	packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
	packages/SystemUI/src/com/android/systemui/tuner/QsTuner.java

packages/SystemUI/src/com/android/systemui/qs/tiles/VolumeTile.java
@@ -1,0 +1,71 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.qs.tiles;
+
+import android.content.Context;
+import android.content.Intent;
+import android.media.AudioManager;
+import android.provider.Settings;
+
+import com.android.internal.logging.MetricsLogger;
+import com.android.systemui.R;
+import com.android.systemui.qs.QSTile;
+
+public class VolumeTile extends QSTile<QSTile.BooleanState> {
+
+    private static final Intent SOUND_SETTINGS = new Intent("android.settings.SOUND_SETTINGS");
+
+    public VolumeTile(Host host) {
+        super(host);
+    }
+
+    @Override
+    protected void handleClick() {
+        MetricsLogger.action(mContext, getMetricsCategory());
+        AudioManager am = (AudioManager) mContext.getSystemService(Context.AUDIO_SERVICE);
+        am.adjustVolume(AudioManager.ADJUST_SAME, AudioManager.FLAG_SHOW_UI);
+    }
+
+    @Override
+    protected void handleLongClick() {
+        mHost.startActivityDismissingKeyguard(SOUND_SETTINGS);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsLogger.VRTOXIN_QS_CONSTANTS;
+    }
+
+    @Override
+    protected void handleUpdateState(BooleanState state, Object arg) {
+        state.visible = true;
+        state.label = mContext.getString(R.string.quick_settings_volume_panel_label);
+        state.contentDescription = mContext.getString(
+                R.string.quick_settings_volume_panel_label);
+        state.icon = ResourceIcon.get(R.drawable.ic_qs_volume_panel); // TODO needs own icon
+    }
+
+    @Override
+    protected BooleanState newTileState() {
+        return new BooleanState();
+    }
+
+    @Override
+    public void setListening(boolean listening) {
+        // Do nothing
+    }
+}