ujwalp15/platform_frameworks_base

Quick Settings: add some more tiles
-Brightness
-ScreenOff
-Screenshot

-Cellular = click to enable/disable and secondary click to show detail
-Bluetooth = add long click to show detailed view

Change-Id: I0b5b2ed03fc99a22627bf9c62760301d353f7e90

packages/SystemUI/src/com/android/systemui/qs/tiles/BrightnessTile.java
@@ -1,0 +1,131 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ * Copyright (C) 2013-2015 The CyanogenMod Project
+ * Copyright 2014-2015 The Euphoria-OS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.qs.tiles;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.database.ContentObserver;
+import android.net.Uri;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.view.View;
+
+import com.android.internal.logging.MetricsLogger;
+import com.android.systemui.R;
+import com.android.systemui.settings.BrightnessController.BrightnessStateChangeCallback;
+import com.android.systemui.qs.QSTile;
+
+/** Quick settings tile: Brightness **/
+public class BrightnessTile extends QSTile<QSTile.BooleanState> {
+
+    private static final String SCREEN_BRIGHTNESS_MODE = "screen_brightness_mode";
+    private static final int SCREEN_BRIGHTNESS_MODE_MANUAL = 0;
+    private static final int SCREEN_BRIGHTNESS_MODE_AUTOMATIC = 1;
+
+    private static final Intent DISPLAY_SETTINGS = new Intent(Settings.ACTION_DISPLAY_SETTINGS);
+    private boolean mListening;
+
+    public BrightnessTile(Host host) {
+        super(host);
+    }
+
+    @Override
+    protected BooleanState newTileState() {
+        return new BooleanState();
+    }
+
+    private ContentObserver mObserver = new ContentObserver(mHandler) {
+        @Override
+        public void onChange(boolean selfChange, Uri uri) {
+            refreshState();
+        }
+    };
+
+    @Override
+    public void setListening(boolean listening) {
+        if (listening) {
+            mContext.getContentResolver().registerContentObserver(
+                    Settings.System.getUriFor(Settings.System.SCREEN_BRIGHTNESS_MODE),
+                    false, mObserver);
+        } else {
+            mContext.getContentResolver().unregisterContentObserver(mObserver);
+        }
+    }
+
+    @Override
+    public void handleClick() {
+        MetricsLogger.action(mContext, getMetricsCategory());
+        mHost.collapsePanels();
+        mContext.startActivityAsUser(new Intent(
+            Intent.ACTION_SHOW_BRIGHTNESS_DIALOG), UserHandle.CURRENT_OR_SELF);
+    }
+
+    @Override
+    protected void handleSecondaryClick() {
+        toggleState();
+        refreshState();
+        //qsCollapsePanel();
+    }
+
+    @Override
+    public void handleLongClick() {
+        toggleState();
+        refreshState();
+        //qsCollapsePanel();
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsLogger.QS_BRIGHTNESS;
+    }
+
+    @Override
+    protected void handleUpdateState(BooleanState state, Object arg) {
+        boolean autoBrightness =
+            getBrightnessState() == Settings.System.SCREEN_BRIGHTNESS_MODE_AUTOMATIC;
+        state.visible = true;
+        state.label = mContext.getString(R.string.quick_settings_brightness_label);
+        state.contentDescription = mContext.getString(
+                R.string.accessibility_quick_settings_brightness);
+        state.icon = autoBrightness
+                ? ResourceIcon.get(R.drawable.ic_qs_brightness_auto_on_alpha)
+                : ResourceIcon.get(R.drawable.ic_qs_brightness_auto_off_alpha);
+    }
+
+    protected void toggleState() {
+        int mode = getBrightnessState();
+        switch (mode) {
+            case SCREEN_BRIGHTNESS_MODE_MANUAL:
+                Settings.System.putInt(mContext.getContentResolver(),
+                    SCREEN_BRIGHTNESS_MODE, SCREEN_BRIGHTNESS_MODE_AUTOMATIC);
+                break;
+            case SCREEN_BRIGHTNESS_MODE_AUTOMATIC:
+                Settings.System.putInt(mContext.getContentResolver(),
+                    SCREEN_BRIGHTNESS_MODE, SCREEN_BRIGHTNESS_MODE_MANUAL);
+                break;
+        }
+    }
+
+    private int getBrightnessState() {
+        return Settings.System.getIntForUser(mContext.getContentResolver(),
+            Settings.System.SCREEN_BRIGHTNESS_MODE, Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL,
+            UserHandle.USER_CURRENT);
+    }
+}

packages/SystemUI/src/com/android/systemui/qs/tiles/ScreenOffTile.java
@@ -1,0 +1,109 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ * Copyright (C) 2013-2015 The CyanogenMod Project
+ * Copyright 2014 SlimRoms project
+ * Copyright 2014-2015 The Euphoria-OS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.qs.tiles;
+
+import android.content.Context;
+import android.provider.Settings;
+import android.provider.Settings.Global;
+import android.hardware.input.InputManager;
+import android.os.Handler;
+import android.os.PowerManager;
+import android.os.SystemClock;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import com.android.internal.logging.MetricsLogger;
+import com.android.systemui.R;
+import com.android.systemui.qs.QSTile;
+
+/** Quick settings tile: Screen off **/
+public class ScreenOffTile extends QSTile<QSTile.BooleanState> {
+
+    private PowerManager mPm;
+    private boolean mListening;
+
+    public ScreenOffTile(Host host) {
+        super(host);
+        mPm = (PowerManager) mContext.getSystemService(Context.POWER_SERVICE);
+    }
+
+    @Override
+    protected BooleanState newTileState() {
+        return new BooleanState();
+    }
+
+    public void setListening(boolean listening) {
+        if (mListening == listening) return;
+        mListening = listening;
+    }
+
+    @Override
+    public void handleClick() {
+        MetricsLogger.action(mContext, getMetricsCategory());
+        mHost.collapsePanels();
+        mPm.goToSleep(SystemClock.uptimeMillis());
+    }
+
+    @Override
+    protected void handleSecondaryClick() {
+        MetricsLogger.action(mContext, getMetricsCategory());
+        mHost.collapsePanels();
+        triggerVirtualKeypress(KeyEvent.KEYCODE_POWER, true);
+    }
+
+    @Override
+    public void handleLongClick() {
+        MetricsLogger.action(mContext, getMetricsCategory());
+        mHost.collapsePanels();
+        triggerVirtualKeypress(KeyEvent.KEYCODE_POWER, true);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsLogger.QS_SCREEN_OFF;
+    }
+
+    @Override
+    protected void handleUpdateState(BooleanState state, Object arg) {
+        state.visible = true;
+        state.label = mContext.getString(R.string.quick_settings_screen_off_label);
+        state.contentDescription = mContext.getString(
+                R.string.accessibility_quick_settings_screen_off);
+        state.icon = ResourceIcon.get(R.drawable.ic_qs_power);
+    }
+
+    private void triggerVirtualKeypress(final int keyCode, final boolean longPress) {
+        new Thread(new Runnable() {
+            public void run() {
+                InputManager im = InputManager.getInstance();
+                KeyEvent keyEvent;
+                if (longPress) {
+                    keyEvent = new KeyEvent(KeyEvent.ACTION_DOWN, keyCode);
+                    keyEvent.changeFlags(keyEvent, KeyEvent.FLAG_FROM_SYSTEM | KeyEvent.FLAG_LONG_PRESS);
+                } else {
+                    keyEvent = new KeyEvent(KeyEvent.ACTION_UP, keyCode);
+                    keyEvent.changeFlags(keyEvent, KeyEvent.FLAG_FROM_SYSTEM);
+                }
+                im.injectInputEvent(keyEvent, InputManager.INJECT_INPUT_EVENT_MODE_WAIT_FOR_RESULT);
+            }
+        }).start();
+    }
+}

SuperKangerRom/android_frameworks_base-AOSP

Change-Id: I05b424fc19f565b68512bbcd61044795e8fa25a6
Conflicts:
	packages/SystemUI/res/values/custom_strings.xml
	packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
	packages/SystemUI/src/com/android/systemui/tuner/QsTuner.java

packages/SystemUI/src/com/android/systemui/qs/tiles/BrightnessTile.java
@@ -1,0 +1,131 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ * Copyright (C) 2013-2015 The CyanogenMod Project
+ * Copyright 2014-2015 The Euphoria-OS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.qs.tiles;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.database.ContentObserver;
+import android.net.Uri;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.view.View;
+
+import com.android.internal.logging.MetricsLogger;
+import com.android.systemui.R;
+import com.android.systemui.settings.BrightnessController.BrightnessStateChangeCallback;
+import com.android.systemui.qs.QSTile;
+
+/** Quick settings tile: Brightness **/
+public class BrightnessTile extends QSTile<QSTile.BooleanState> {
+
+    private static final String SCREEN_BRIGHTNESS_MODE = "screen_brightness_mode";
+    private static final int SCREEN_BRIGHTNESS_MODE_MANUAL = 0;
+    private static final int SCREEN_BRIGHTNESS_MODE_AUTOMATIC = 1;
+
+    private static final Intent DISPLAY_SETTINGS = new Intent(Settings.ACTION_DISPLAY_SETTINGS);
+    private boolean mListening;
+
+    public BrightnessTile(Host host) {
+        super(host);
+    }
+
+    @Override
+    protected BooleanState newTileState() {
+        return new BooleanState();
+    }
+
+    private ContentObserver mObserver = new ContentObserver(mHandler) {
+        @Override
+        public void onChange(boolean selfChange, Uri uri) {
+            refreshState();
+        }
+    };
+
+    @Override
+    public void setListening(boolean listening) {
+        if (listening) {
+            mContext.getContentResolver().registerContentObserver(
+                    Settings.System.getUriFor(Settings.System.SCREEN_BRIGHTNESS_MODE),
+                    false, mObserver);
+        } else {
+            mContext.getContentResolver().unregisterContentObserver(mObserver);
+        }
+    }
+
+    @Override
+    public void handleClick() {
+        MetricsLogger.action(mContext, getMetricsCategory());
+        mHost.collapsePanels();
+        mContext.startActivityAsUser(new Intent(
+            Intent.ACTION_SHOW_BRIGHTNESS_DIALOG), UserHandle.CURRENT_OR_SELF);
+    }
+
+    @Override
+    protected void handleSecondaryClick() {
+        toggleState();
+        refreshState();
+        //qsCollapsePanel();
+    }
+
+    @Override
+    public void handleLongClick() {
+        toggleState();
+        refreshState();
+        //qsCollapsePanel();
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsLogger.VRTOXIN_QS_CONSTANTS;
+    }
+
+    @Override
+    protected void handleUpdateState(BooleanState state, Object arg) {
+        boolean autoBrightness =
+            getBrightnessState() == Settings.System.SCREEN_BRIGHTNESS_MODE_AUTOMATIC;
+        state.visible = true;
+        state.label = mContext.getString(R.string.quick_settings_brightness_label);
+        state.contentDescription = mContext.getString(
+                R.string.accessibility_quick_settings_brightness);
+        state.icon = autoBrightness
+                ? ResourceIcon.get(R.drawable.ic_qs_brightness_auto_on_alpha)
+                : ResourceIcon.get(R.drawable.ic_qs_brightness_auto_off_alpha);
+    }
+
+    protected void toggleState() {
+        int mode = getBrightnessState();
+        switch (mode) {
+            case SCREEN_BRIGHTNESS_MODE_MANUAL:
+                Settings.System.putInt(mContext.getContentResolver(),
+                    SCREEN_BRIGHTNESS_MODE, SCREEN_BRIGHTNESS_MODE_AUTOMATIC);
+                break;
+            case SCREEN_BRIGHTNESS_MODE_AUTOMATIC:
+                Settings.System.putInt(mContext.getContentResolver(),
+                    SCREEN_BRIGHTNESS_MODE, SCREEN_BRIGHTNESS_MODE_MANUAL);
+                break;
+        }
+    }
+
+    private int getBrightnessState() {
+        return Settings.System.getIntForUser(mContext.getContentResolver(),
+            Settings.System.SCREEN_BRIGHTNESS_MODE, Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL,
+            UserHandle.USER_CURRENT);
+    }
+}

packages/SystemUI/src/com/android/systemui/qs/tiles/ScreenOffTile.java
@@ -1,0 +1,109 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ * Copyright (C) 2013-2015 The CyanogenMod Project
+ * Copyright 2014 SlimRoms project
+ * Copyright 2014-2015 The Euphoria-OS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.qs.tiles;
+
+import android.content.Context;
+import android.provider.Settings;
+import android.provider.Settings.Global;
+import android.hardware.input.InputManager;
+import android.os.Handler;
+import android.os.PowerManager;
+import android.os.SystemClock;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import com.android.internal.logging.MetricsLogger;
+import com.android.systemui.R;
+import com.android.systemui.qs.QSTile;
+
+/** Quick settings tile: Screen off **/
+public class ScreenOffTile extends QSTile<QSTile.BooleanState> {
+
+    private PowerManager mPm;
+    private boolean mListening;
+
+    public ScreenOffTile(Host host) {
+        super(host);
+        mPm = (PowerManager) mContext.getSystemService(Context.POWER_SERVICE);
+    }
+
+    @Override
+    protected BooleanState newTileState() {
+        return new BooleanState();
+    }
+
+    public void setListening(boolean listening) {
+        if (mListening == listening) return;
+        mListening = listening;
+    }
+
+    @Override
+    public void handleClick() {
+        MetricsLogger.action(mContext, getMetricsCategory());
+        mHost.collapsePanels();
+        mPm.goToSleep(SystemClock.uptimeMillis());
+    }
+
+    @Override
+    protected void handleSecondaryClick() {
+        MetricsLogger.action(mContext, getMetricsCategory());
+        mHost.collapsePanels();
+        triggerVirtualKeypress(KeyEvent.KEYCODE_POWER, true);
+    }
+
+    @Override
+    public void handleLongClick() {
+        MetricsLogger.action(mContext, getMetricsCategory());
+        mHost.collapsePanels();
+        triggerVirtualKeypress(KeyEvent.KEYCODE_POWER, true);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsLogger.VRTOXIN_QS_CONSTANTS;
+    }
+
+    @Override
+    protected void handleUpdateState(BooleanState state, Object arg) {
+        state.visible = true;
+        state.label = mContext.getString(R.string.quick_settings_screen_off_label);
+        state.contentDescription = mContext.getString(
+                R.string.accessibility_quick_settings_screen_off);
+        state.icon = ResourceIcon.get(R.drawable.ic_qs_power);
+    }
+
+    private void triggerVirtualKeypress(final int keyCode, final boolean longPress) {
+        new Thread(new Runnable() {
+            public void run() {
+                InputManager im = InputManager.getInstance();
+                KeyEvent keyEvent;
+                if (longPress) {
+                    keyEvent = new KeyEvent(KeyEvent.ACTION_DOWN, keyCode);
+                    keyEvent.changeFlags(keyEvent, KeyEvent.FLAG_FROM_SYSTEM | KeyEvent.FLAG_LONG_PRESS);
+                } else {
+                    keyEvent = new KeyEvent(KeyEvent.ACTION_UP, keyCode);
+                    keyEvent.changeFlags(keyEvent, KeyEvent.FLAG_FROM_SYSTEM);
+                }
+                im.injectInputEvent(keyEvent, InputManager.INJECT_INPUT_EVENT_MODE_WAIT_FOR_RESULT);
+            }
+        }).start();
+    }
+}