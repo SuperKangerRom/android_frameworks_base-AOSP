CyanogenMod/android_frameworks_base

SystemUI : Add adb over network tile
Change-Id: Ib1fa0c87562ff3a077bf9c7283ce81fc4a8271db

core/java/com/android/internal/util/cm/QSConstants.java
@@ -38,6 +38,7 @@ public class QSConstants {
     public static final String TILE_APN = "apn";
     public static final String TILE_PROFILES = "profiles";
     public static final String TILE_PERFORMANCE = "performance";
+    public static final String TILE_ADB_NETWORK = "adb_network";
 
     // Order matters
     protected static final ArrayList<String> TILES_DEFAULT = new ArrayList<String>();
@@ -66,5 +67,6 @@ public class QSConstants {
         TILES_AVAILABLE.add(TILE_APN);
         TILES_AVAILABLE.add(TILE_PROFILES);
         TILES_AVAILABLE.add(TILE_PERFORMANCE);
+        TILES_AVAILABLE.add(TILE_ADB_NETWORK);
     }
 }

SuperKangerRom/android_frameworks_base-AOSP

core/java/com/android/internal/util/vrtoxin/QSConstants.java
@@ -106,5 +106,6 @@ public class QSConstants {
         STATIC_TILES_AVAILABLE.add(TILE_REBOOT);
         STATIC_TILES_AVAILABLE.add(TILE_SYNC);
         STATIC_TILES_AVAILABLE.add(TILE_MUSIC);
+        STATIC_TILES_AVAILABLE.add(TILE_ADB_NETWORK);
     }
 }