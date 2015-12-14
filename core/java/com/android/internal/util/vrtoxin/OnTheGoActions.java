CyanideL/android_frameworks_base

On the go mode 2/2
Credits to nameless for the feature and fusion for the forward port, still needs some work

core/java/com/android/internal/util/cyanide/OnTheGoActions.java
@@ -1,0 +1,51 @@
+/*
+* <!--
+*    Copyright (C) 2014 The NamelessROM Project
+*
+*    This program is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*
+*    This program is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+* -->
+*/
+
+package com.android.internal.util.cyanide;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+
+public class OnTheGoActions {
+
+    public static final String ACTION_ONTHEGO_TOGGLE = "action_onthego_toggle";
+
+    public static void processAction(final Context context, final String action) {
+
+        if (action == null || action.isEmpty()) {
+            return;
+        }
+
+        if (ACTION_ONTHEGO_TOGGLE.equals(action)) {
+            actionOnTheGoToggle(context);
+        }
+
+    }
+
+    private static void actionOnTheGoToggle(final Context context) {
+        final ComponentName cn = new ComponentName("com.android.systemui",
+                "com.android.systemui.cyanide.onthego.OnTheGoService");
+        final Intent startIntent = new Intent();
+        startIntent.setComponent(cn);
+        startIntent.setAction("start");
+        context.startService(startIntent);
+    }
+
+}

core/java/com/android/internal/util/cyanide/OnTheGoUtils.java
@@ -1,0 +1,106 @@
+/*
+* <!--
+*    Copyright (C) 2014 The NamelessROM Project
+*
+*    This program is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*
+*    This program is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+* -->
+*/
+
+package com.android.internal.util.cyanide;
+
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.util.Log;
+
+import java.util.List;
+
+public class OnTheGoUtils {
+
+    private static final String TAG = "OnTheGoUtils";
+
+    /**
+     * Checks if a specific package is installed.
+     *
+     * @param context     The context to retrieve the package manager
+     * @param packageName The name of the package
+     * @return Whether the package is installed or not.
+     */
+    public static boolean isPackageInstalled(Context context, String packageName) {
+        PackageManager pm = context.getPackageManager();
+        try {
+            if (pm != null) {
+                List<ApplicationInfo> packages = pm.getInstalledApplications(0);
+                for (ApplicationInfo packageInfo : packages) {
+                    if (packageInfo.packageName.equals(packageName)) {
+                        return true;
+                    }
+                }
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "Error: " + e.getMessage());
+        }
+        return false;
+    }
+
+    /**
+     * Checks if a specific service is running.
+     *
+     * @param context     The context to retrieve the activity manager
+     * @param serviceName The name of the service
+     * @return Whether the service is running or not
+     */
+    public static boolean isServiceRunning(Context context, String serviceName) {
+        ActivityManager activityManager = (ActivityManager) context
+                .getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningServiceInfo> services = activityManager
+                .getRunningServices(Integer.MAX_VALUE);
+
+        if (services != null) {
+            for (ActivityManager.RunningServiceInfo info : services) {
+                if (info.service != null) {
+                    if (info.service.getClassName() != null && info.service.getClassName()
+                            .equalsIgnoreCase(serviceName)) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Check if system has a camera.
+     *
+     * @param context
+     * @return
+     */
+    public static boolean hasCamera(final Context context) {
+        final PackageManager pm = context.getPackageManager();
+        return pm != null && pm.hasSystemFeature(PackageManager.FEATURE_CAMERA);
+    }
+
+    /**
+     * Check if system has a front camera.
+     *
+     * @param context
+     * @return
+     */
+    public static boolean hasFrontCamera(final Context context) {
+        final PackageManager pm = context.getPackageManager();
+        return pm != null && pm.hasSystemFeature(PackageManager.FEATURE_CAMERA_FRONT);
+    }
+}

policy/src/com/android/internal/policy/impl/GlobalActions.java
@@ -106,6 +106,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+import com.android.internal.util.cyanide.OnTheGoActions;
 
 /**
  * Helper to show the global actions dialog.  Each item is an {@link Action} that
@@ -284,6 +285,10 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
 
             Drawable icon = PolicyHelper.getPowerMenuIconImage(mContext, actionKey, config.getIcon());
 
+            // On-The-Go, if enabled
+            boolean showOnTheGo = Settings.System.getBoolean(mContext.getContentResolver(),
+                    Settings.System.POWER_MENU_ONTHEGO_ENABLED, false);
+
             if (actionKey.equals(PolicyConstants.ACTION_POWER_OFF)) {
                 mItems.add(getPowerAction(icon));
             } else if (actionKey.equals(PolicyConstants.ACTION_REBOOT)) {
@@ -301,6 +306,10 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
                 }
             } else if (actionKey.equals(PolicyConstants.ACTION_LOCKDOWN)) {
                 mItems.add(getLockdownAction(icon));
+            } else if (actionKey.equals(PolicyConstants.ACTION_ONTHEGO)) {
+                if (showOnTheGo) {
+                    mItems.add(getOnTheGoAction(icon));
+                }
             } else if (actionKey != null) {
                 // must be a screenshot, custom app or action shorcut
                 mItems.add(
@@ -478,6 +487,29 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
         };
     }
 
+
+    private Action getOnTheGoAction(Drawable icon) {
+        return new SinglePressAction(icon, R.string.global_action_onthego) {
+
+             public void onPress() {
+                 OnTheGoActions.processAction(mContext,
+                         OnTheGoActions.ACTION_ONTHEGO_TOGGLE);
+             }
+
+             public boolean onLongPress() {
+                 return false;
+             }
+
+             public boolean showDuringKeyguard() {
+                 return true;
+             }
+
+             public boolean showBeforeProvisioning() {
+                 return true;
+             }
+        };
+    }
+
     private UserInfo getCurrentUser() {
         try {
             return ActivityManagerNative.getDefault().getCurrentUser();
@@ -541,6 +573,14 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
         }
     }
 
+    private void startOnTheGo() {
+        final ComponentName cn = new ComponentName("com.android.systemui",
+                "com.android.systemui.fusion.onthego.OnTheGoService");
+        final Intent startIntent = new Intent();
+        startIntent.setComponent(cn);
+        startIntent.setAction("start");
+        mContext.startService(startIntent);
+    }
 
     private void prepareDialog() {
         refreshSilentMode();

SuperKangerRom/android_frameworks_base-AOSP

core/java/com/android/internal/util/vrtoxin/OnTheGoActions.java
@@ -1,0 +1,51 @@
+/*
+* <!--
+*    Copyright (C) 2014 The NamelessROM Project
+*
+*    This program is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*
+*    This program is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+* -->
+*/
+
+package com.android.internal.util.vrtoxin;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+
+public class OnTheGoActions {
+
+    public static final String ACTION_ONTHEGO_TOGGLE = "action_onthego_toggle";
+
+    public static void processAction(final Context context, final String action) {
+
+        if (action == null || action.isEmpty()) {
+            return;
+        }
+
+        if (ACTION_ONTHEGO_TOGGLE.equals(action)) {
+            actionOnTheGoToggle(context);
+        }
+
+    }
+
+    private static void actionOnTheGoToggle(final Context context) {
+        final ComponentName cn = new ComponentName("com.android.systemui",
+                "com.android.systemui.vrtoxin.onthego.OnTheGoService");
+        final Intent startIntent = new Intent();
+        startIntent.setComponent(cn);
+        startIntent.setAction("start");
+        context.startService(startIntent);
+    }
+
+}

core/java/com/android/internal/util/vrtoxin/OnTheGoUtils.java
@@ -1,0 +1,106 @@
+/*
+* <!--
+*    Copyright (C) 2014 The NamelessROM Project
+*
+*    This program is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*
+*    This program is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+* -->
+*/
+
+package com.android.internal.util.vrtoxin;
+
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.util.Log;
+
+import java.util.List;
+
+public class OnTheGoUtils {
+
+    private static final String TAG = "OnTheGoUtils";
+
+    /**
+     * Checks if a specific package is installed.
+     *
+     * @param context     The context to retrieve the package manager
+     * @param packageName The name of the package
+     * @return Whether the package is installed or not.
+     */
+    public static boolean isPackageInstalled(Context context, String packageName) {
+        PackageManager pm = context.getPackageManager();
+        try {
+            if (pm != null) {
+                List<ApplicationInfo> packages = pm.getInstalledApplications(0);
+                for (ApplicationInfo packageInfo : packages) {
+                    if (packageInfo.packageName.equals(packageName)) {
+                        return true;
+                    }
+                }
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "Error: " + e.getMessage());
+        }
+        return false;
+    }
+
+    /**
+     * Checks if a specific service is running.
+     *
+     * @param context     The context to retrieve the activity manager
+     * @param serviceName The name of the service
+     * @return Whether the service is running or not
+     */
+    public static boolean isServiceRunning(Context context, String serviceName) {
+        ActivityManager activityManager = (ActivityManager) context
+                .getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningServiceInfo> services = activityManager
+                .getRunningServices(Integer.MAX_VALUE);
+
+        if (services != null) {
+            for (ActivityManager.RunningServiceInfo info : services) {
+                if (info.service != null) {
+                    if (info.service.getClassName() != null && info.service.getClassName()
+                            .equalsIgnoreCase(serviceName)) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Check if system has a camera.
+     *
+     * @param context
+     * @return
+     */
+    public static boolean hasCamera(final Context context) {
+        final PackageManager pm = context.getPackageManager();
+        return pm != null && pm.hasSystemFeature(PackageManager.FEATURE_CAMERA);
+    }
+
+    /**
+     * Check if system has a front camera.
+     *
+     * @param context
+     * @return
+     */
+    public static boolean hasFrontCamera(final Context context) {
+        final PackageManager pm = context.getPackageManager();
+        return pm != null && pm.hasSystemFeature(PackageManager.FEATURE_CAMERA_FRONT);
+    }
+}

services/core/java/com/android/server/policy/GlobalActions.java
@@ -99,6 +99,8 @@ import com.vrtoxin.util.Helpers;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.android.internal.util.vrtoxin.OnTheGoActions;
+
 /**
  * Helper to show the global actions dialog.  Each item is an {@link Action} that
  * may show depending on whether the keyguard is showing, and whether the device
@@ -291,8 +293,12 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
                 PowerMenuHelper.getPowerMenuConfig(mContext);
 
         mItems = new ArrayList<Action>();
-
         for (final ActionConfig config : powerMenuConfig) {
+
+            // On-The-Go, if enabled
+            boolean showOnTheGo = Settings.System.getBoolean(mContext.getContentResolver(),
+                    Settings.System.POWER_MENU_ONTHEGO_ENABLED, false);
+
             if (config.getClickAction().equals(PowerMenuConstants.ACTION_POWER_OFF)) {
                 mItems.add(getPowerAction());
             } else if (config.getClickAction().equals(PowerMenuConstants.ACTION_REBOOT)) {
@@ -330,6 +336,10 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
                 mItems.add(getScreenRecordAction());
             } else if (config.getClickAction().equals(PowerMenuConstants.ACTION_SCREENSHOT)) {
                 mItems.add(getScreenshotAction());
+            } else if (config.getClickAction().equals(PowerMenuConstants.ACTION_ONTHEGO)) {
+                if (showOnTheGo) {
+                    mItems.add(getOnTheGoAction());
+                }
             } else if (config.getClickAction().equals(PowerMenuConstants.ACTION_SOUND) && mShowSilentToggle) {
                 if (!mHasVibrator) {
                     mSilentModeAction = new SilentModeToggleAction();
@@ -823,6 +833,27 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
             public boolean showBeforeProvisioning() {
                 return true;
             }
+        };
+    }
+
+    private Action getOnTheGoAction() {
+        return new SinglePressAction(com.android.internal.R.drawable.ic_lock_onthego, R.string.global_action_onthego) {
+
+             @Override
+             public void onPress() {
+                 OnTheGoActions.processAction(mContext,
+                         OnTheGoActions.ACTION_ONTHEGO_TOGGLE);
+             }
+
+             @Override
+             public boolean showDuringKeyguard() {
+                 return true;
+             }
+
+             @Override
+             public boolean showBeforeProvisioning() {
+                 return true;
+             }
         };
     }
 