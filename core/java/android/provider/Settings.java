973bf1766aa10ff94047246808aa1e736859173a
NL-BlackDragon <blackdragon.vrtoxin@gmail.com>
2015-12-18 00:06:31 +0100

    Breathing SMS: remove @hide
    Signed-off-by: NL-BlackDragon <blackdragon.vrtoxin@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3446,7 +3446,7 @@ public final class Settings {
 
         /**
          * Give SMS notifications a breathing effect
-         * @hide
+         *
          */
         public static final String KEY_SMS_BREATH = "sms_breath";
 
