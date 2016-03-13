bionic-linux/android_frameworks_base

Add an AM option to start with allocation tracking
The new option "--track-allocation" is to work with the new
allocation tracker in ART.

Bug:20037135
Change-Id: Ic5f8945ab4c1f167c27b05ad0d11d04bac680c1f

services/core/java/com/android/server/am/ActivityStackSupervisor.java
@@ -823,6 +823,12 @@ public final class ActivityStackSupervisor implements DisplayListener {
                 }
             }
 
+            if ((startFlags&ActivityManager.START_FLAG_TRACK_ALLOCATION) != 0) {
+              if (!aInfo.processName.equals("system")) {
+                  mService.setTrackAllocationApp(aInfo.applicationInfo, aInfo.processName);
+              }
+            }
+
             if (profilerInfo != null) {
                 if (!aInfo.processName.equals("system")) {
                     mService.setProfileApp(aInfo.applicationInfo, aInfo.processName, profilerInfo);

SuperKangerRom/android_frameworks_base-AOSP

services/core/java/com/android/server/am/ActivityStackSupervisor.java
@@ -890,20 +890,20 @@ public final class ActivityStackSupervisor implements DisplayListener {
                     aInfo.applicationInfo.packageName, aInfo.name));
 
             // Don't debug things in the system process
-            if ((startFlags&ActivityManager.START_FLAG_DEBUG) != 0) {
-                if (!aInfo.processName.equals("system")) {
+            if (!aInfo.processName.equals("system")) {
+                if ((startFlags & ActivityManager.START_FLAG_DEBUG) != 0) {
                     mService.setDebugApp(aInfo.processName, true, false);
                 }
-            }
 
-            if ((startFlags&ActivityManager.START_FLAG_OPENGL_TRACES) != 0) {
-                if (!aInfo.processName.equals("system")) {
+                if ((startFlags & ActivityManager.START_FLAG_OPENGL_TRACES) != 0) {
                     mService.setOpenGlTraceApp(aInfo.applicationInfo, aInfo.processName);
                 }
-            }
 
-            if (profilerInfo != null) {
-                if (!aInfo.processName.equals("system")) {
+                if ((startFlags & ActivityManager.START_FLAG_TRACK_ALLOCATION) != 0) {
+                    mService.setTrackAllocationApp(aInfo.applicationInfo, aInfo.processName);
+                }
+
+                if (profilerInfo != null) {
                     mService.setProfileApp(aInfo.applicationInfo, aInfo.processName, profilerInfo);
                 }
             }